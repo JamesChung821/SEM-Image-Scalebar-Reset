410b2881da09cac14f456703f1913c7fe4931ee8
Cheng-Chu Chung <cheng-chu.chung@stonybrook.edu>
2023-06-11 01:54:41 -0400

    add Hitachi and old Helios info index

--- SEM_scalebar_reset.py
+++ SEM_scalebar_reset.py
@@ -35,7 +35,7 @@ def streamlit_mode():
     show_reset_image = st.sidebar.checkbox("Show reset image")
 
     # File upload
-    uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png", "tif"])
+    uploaded_file = st.file_uploader("Choose an image file", type=["tif"])  # "jpg", "jpeg", "png", "tif", "tiff"
 
     sem_manufacturer = st.sidebar.selectbox(
         'SEM Manufacturer',
@@ -53,31 +53,39 @@ def streamlit_mode():
         if show_original_image:
             st.image(original_image, caption="Uploaded Image", output_format='PNG')
 
+        # Display the uploaded image info
         dpi = original_image.info['dpi'][0]
         x_pixel = original_image.size[0]
         length = x_pixel / dpi * 25.4
-        print(x_pixel, dpi, length)
+        print(f'x pixels: {x_pixel}, dpi: {dpi}, length: {length}')
 
         img = np.array(original_image)  # Convert to numpy array because pytesseract only accepts numpy array
         print(img.shape)
 
         black_row_index = img.shape[0]  # Initialize the black row index
 
-        print(img[black_row_index-10, :])
+        print(img[black_row_index-10, :])   # Check the last 10 rows (pixels) of the image
 
         for index, black_row in enumerate(img):     # Find the first black row or white row to crop the image
-            if black_row[:5].mean() == 11822 or black_row[:50].mean() == 255 or black_row[:50].mean() == 46 or black_row[:50].mean() == 257:
+            if black_row[1:5].mean() == 11822 \
+                    or black_row[1:50].mean() == 255 \
+                    or black_row[1:50].mean() == 46 \
+                    or black_row[1:50].mean() == 257 \
+                    or black_row[1:50].mean() == 0:
                 black_row_index = index
                 print(black_row)
-                print(black_row_index)
+                print('black row index', black_row_index)
                 break
 
+        # Crop the image and extract the text from the image at the bottom info bar
         text = pytesseract.image_to_string(
-            img[black_row_index-100:], config='--psm 6').replace('\n', ' ')    # Extract the text from the image at the bottom info bar
+            img[black_row_index-100:], config='--psm 6').replace('\n', ' ')
         print(f'Text: {text}')
 
+        # Search the magnification from the text
         magnification = search_magnification(sem_manufacturer, text)
 
+        # Display the scalebar
         scalebar = ScaleBar(length/magnification/x_pixel, 'mm',
                             length_fraction=0.25,
                             location='lower right',
@@ -96,6 +104,7 @@ def streamlit_mode():
                           sep=5,
                           font_properties={'size': 'small'})
 
+        # Display the reset image
         fig, ax = plt.subplots()
         ax.set_axis_off()
         plt.gca().add_artist(scalebar)
@@ -103,6 +112,7 @@ def streamlit_mode():
         if show_reset_image:
             st.pyplot(fig)
 
+        # Save the reset image
         fig = io.BytesIO()  # Create a new in-memory file object for the plot
         plt.savefig(fig, format='png', dpi=600, bbox_inches='tight', pad_inches=0)
         btn = st.download_button(
@@ -171,6 +181,12 @@ def local_mode():
 
 
 def search_magnification(sem_manufacturer, text):
+    """
+    Search the magnification from the text
+    :param sem_manufacturer: string, the manufacturer of the SEM
+    :param text: string, the text extracted from the image
+    :return: int, the magnification
+    """
     if sem_manufacturer == 'Helios':
         magnification_head = 0
         magnification_tail = text.find('x')-1
@@ -190,6 +206,19 @@ def search_magnification(sem_manufacturer, text):
         print(magnification_head, magnification_tail)
 
         magnification = int(text[magnification_head+1:magnification_tail].replace(' ', '').replace(',', ''))  # Extract the magnification
+        print(magnification)
+        return magnification
+
+    elif sem_manufacturer == 'Hitachi':
+        magnification_head = text.find('x')
+        magnification_tail = text.find(' ', magnification_head+2)
+        print(magnification_head, magnification_tail)
+
+        magnification = text[magnification_head+1:magnification_tail].replace(' ', '')  # Extract the magnification
+        if 'k' in magnification:
+            magnification = int(float(magnification[:-1]) * 1000)
+        else:
+            magnification = int(magnification)
         print(magnification)
         return magnification
 
